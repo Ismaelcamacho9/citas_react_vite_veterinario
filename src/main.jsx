497eeeaed8017bf9978bda1a169e2b902b5f220e
ismaelcamacho9 <ismaelcamacholinares@gmail.com>
2023-08-15 18:38:54 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
